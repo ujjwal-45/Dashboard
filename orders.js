30a7439c222da8437450103273f5491322a0c283
Ujjwal <ujjwalchoudhary065@gmail.com>
2024-04-05 19:50:18 +0530

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "Java Chip",
+    productNumber: "00028",
+    paymentStatus: "Due",
+    status: "Pending",
+  },
+  {
+    productName: 'Choco Lava Cake',
+    productNumber: '00056',
+    paymentStatus: 'Refunded',
+    status: 'Declined',
+  },
+  {
+    productName: "Coconut Cookies",
+    productNumber: "00113",
+    paymentStatus: "Paid",
+    status: "Active",
+  },
+];
